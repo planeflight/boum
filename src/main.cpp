c23865d4d58cbf9e640937f9935d17fa92ae1567
planeflight <olivier.lvannier@gmail.com>
2025-02-06 19:49:39 -0800

    Load music with raylib, change readme

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,50 @@
 #include <raylib.h>
 
-#include "util/types.hpp"
+#include <iostream>
 
-#ifdef PLATFORM_WEB
-#include <emscripten/emscripten.h>
-#endif
+#include "util/types.hpp"
 
 static constexpr i32 window_width = 1600, window_height = 900;
 
-struct Global {
-    Texture texture;
-} g;
-
-void init() {
-    g.texture = LoadTexture("./res/scarfy.png");
+void processor(void *buffer_data, u32 frames) {
+    // i32 *data = buffer_data;
 }
 
-void input(f32 dt) {}
+i32 main() {
+    InitWindow(window_width, window_height, "Boum");
+    InitAudioDevice();
+    Music music = LoadMusicStream("./res/dungeon.ogg");
+    SetMusicVolume(music, 1.5f);
+    PlayMusicStream(music);
+    AttachAudioStreamProcessor(music.stream, processor);
+    printf("%i %i %i %i\n",
+           music.stream.sampleSize,
+           music.stream.sampleRate,
+           music.stream.channels,
+           music.frameCount);
 
-void update(f32 dt) {}
-
-void render(f32 dt) {
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-
-    DrawText("SOME TEXT!!", 190, 200, 20, LIGHTGRAY);
-
-    DrawTexture(g.texture, 0, 0, WHITE);
-    EndDrawing();
-}
-
-void quit() {
-    UnloadTexture(g.texture);
-    CloseWindow();
-}
-
-void frame();
-
-int main() {
-    InitWindow(window_width, window_height, "GAME");
-    init();
-
-#ifdef PLATFORM_WEB
-    emscripten_set_main_loop(frame, 0, 1);
-#else
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
-        frame();
-    }
-#endif
+        f32 dt = GetFrameTime();
+        UpdateMusicStream(music);
+        if (IsKeyPressed(KEY_SPACE)) {
+            if (IsMusicStreamPlaying(music)) {
+                PauseMusicStream(music);
+            } else {
+                ResumeMusicStream(music);
+            }
+        }
 
-    quit();
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText("SOME TEXT!!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseAudioDevice();
+    CloseWindow();
 
     return 0;
-}
-
-void frame() {
-    f32 dt = GetFrameTime();
-    input(dt);
-    update(dt);
-    render(dt);
 }
