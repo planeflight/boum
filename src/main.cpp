9a816f343808b03b25a9177913075c0e5757b197
planeflight <olivier.lvannier@gmail.com>
2025-02-10 17:56:48 -0800

    decompose samples to individual amplitudes, draw sound wave to screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,53 @@
 #include <raylib.h>
 
+#include <cmath>
+#include <cstring>
 #include <iostream>
 
 #include "util/types.hpp"
 
 static constexpr i32 window_width = 1600, window_height = 900;
 
-void processor(void *buffer_data, u32 frames) {
-    // i32 *data = buffer_data;
+static constexpr u32 num_frames = 1500;
+f32 music_frames[num_frames * 2] = {0.0f};
+u32 current_frames = 0;
+
+void processor(void *buffer, u32 frames) {
+    f32 *samples = (f32 *)buffer; // Samples internally stored as <float>s
+    // if overflow
+    if (current_frames + frames >= num_frames) {
+        std::memmove(music_frames,
+                     music_frames + (frames) * 2,
+                     sizeof(f32) * (num_frames - frames) * 2);
+        current_frames = num_frames - frames;
+    }
+
+    std::memcpy(
+        music_frames + current_frames * 2, samples, sizeof(f32) * frames * 2);
+    current_frames += frames;
+    // if (frames <= num_frames - current_frames) {
+    //     memcpy(music_frames + current_frames * 2,
+    //            samples,
+    //            sizeof(f32) * frames * 2);
+    //     current_frames += frames;
+    // } else if (frames <= num_frames) {
+    //     memmove(music_frames,
+    //             music_frames + frames * 2,
+    //             sizeof(f32) * 2 * (num_frames - frames));
+    //     memcpy(music_frames + (num_frames - frames) * 2,
+    //            samples,
+    //            sizeof(f32) * 2 * frames);
+    // } else {
+    //     memcpy(music_frames, samples, sizeof(f32) * 2 * num_frames);
+    //     current_frames = num_frames;
+    // }
 }
 
 i32 main() {
     InitWindow(window_width, window_height, "Boum");
     InitAudioDevice();
-    Music music = LoadMusicStream("./res/dungeon.ogg");
+    // Music music = LoadMusicStream("./res/dungeon.ogg");
+    Music music = LoadMusicStream("./res/sample.mp3");
     SetMusicVolume(music, 1.5f);
     PlayMusicStream(music);
     AttachAudioStreamProcessor(music.stream, processor);
@@ -26,7 +60,7 @@ i32 main() {
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
         f32 dt = GetFrameTime();
-        UpdateMusicStream(music);
+        UpdateMusicStream(music); // Update music buffer with new stream data
         if (IsKeyPressed(KEY_SPACE)) {
             if (IsMusicStreamPlaying(music)) {
                 PauseMusicStream(music);
@@ -38,10 +72,16 @@ i32 main() {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawText("SOME TEXT!!", 190, 200, 20, LIGHTGRAY);
+        for (i32 i = 0; i < num_frames; i++) {
+            f32 t = music_frames[i * 2];
+            DrawRectangle(
+                1 + i, window_height / 2 - (i32)(t * 32), 1, t * 64, GOLD);
+        }
 
         EndDrawing();
     }
+
+    DetachAudioStreamProcessor(music.stream, processor);
 
     CloseAudioDevice();
     CloseWindow();
